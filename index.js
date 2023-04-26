1d672bae452ce09b6a686ea4afbb2a8738c8dbcf
prema3labglo <prema.labglo@gmail.com>
2023-04-26 14:44:03 +0530

    deploy

--- index.js
+++ index.js
@@ -7,8 +7,5 @@ app.get('/', function (req, res) {
 });  
 
 
-var server = app.listen(4000, function () {  
-  var host = server.address().address;  
-  var port = server.address().port;  
-  console.log('Example app listening at http://%s:%s', host, port);  
-});  
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running on port ${port}`)); 
